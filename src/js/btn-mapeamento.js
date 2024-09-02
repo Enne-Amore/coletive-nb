e3e5605323bb6cbf37a66f6a33cb2fe9d844aa44
Enne Jim Pessoa <EnneFofe10@gmail.com>
2024-09-02 12:31:47 -0300

    Ajustes - Parte 5

---
+++ src/js/btn-mapeamento.js
@@ -0,0 +1,17 @@
+// Chamada do botão
+const btn_faca_parte = document.querySelectorAll('.faca-parte')
+
+btn_faca_parte.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // Chamada da seção
+        const btn_mapeamento = document.getElementById('btn-mapeamento')
+    
+        // Animação da seção
+        btn_mapeamento.style.animation = 'btn-mapeamento .8s ease-out .7s'
+    
+        // Remover animação
+        setTimeout(() => {
+            btn_mapeamento.style.animation = 'none'
+        }, 2000)
+    })
+})
