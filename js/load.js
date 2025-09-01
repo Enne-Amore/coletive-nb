3776abf9b6e077013a18837d3f33a7c649981f49
Enne Jim Pessoa <ennefofe10@gmail.com>
2025-09-01 09:50:00 -0300

    fix: loaded page animation

--- js/load.js
+++ js/load.js
@@ -4,6 +4,7 @@ const main = document.querySelector("#conteudo")
 const footer = document.querySelector("#rodape")
 
 window.addEventListener("load", () => {
+  html.style.animation = "aparecer 0.5s ease-out"
   load.style.display = "none"
   header.style.display = "flex"
   main.style.display = "block"
